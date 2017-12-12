112898c865b382bce89a9269e526299d76e37e96
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-11 19:40:15 -0700

    category new feature test

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,30 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.title} added!"
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def category_params
+     params.require(:category).permit(:title)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+end
