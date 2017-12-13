2b0d5336ceb2be5ec6c34b56ef663b5e1334cff7
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-13 11:53:14 -0700

    is invalid without name or position

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "contact validations" do 
+  it "is invalid without a name" do
+
+  contact = Contact.create(position: "CTO", email: "T@aol.com" )
+
+  expect(contact).to be_invalid
+  end
+
+  it "is invalid without a position" do 
+
+    contact = Contact.create(name: "Timo", email: "T@gmail.com")
+
+    expect(contact).to be_invalid
+  end
+end
