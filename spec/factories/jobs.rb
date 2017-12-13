41d6d2b66ef1bed4729493029dd8168727d934d1
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-12 17:37:15 -0700

    comment submit works

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :job do
-    sequence(:name) {|n| "Job#{n}"}
+    sequence(:title) {|n| "Job#{n}"}
     sequence(:level_of_interest) {|n| n}
     sequence(:city) {|n| "City#{n}"}
+    company
   end
 end
