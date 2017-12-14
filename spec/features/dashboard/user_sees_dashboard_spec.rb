a59c8f4741089cb82093e99dd2e1bb023ffc196c
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-14 08:57:41 -0700

    final

--- spec/features/dashboard/user_sees_dashboard_spec.rb
+++ spec/features/dashboard/user_sees_dashboard_spec.rb
@@ -14,7 +14,33 @@ describe "User sees dashboard" do
     expect(page).to have_content("Job Count By Interest Level:")
     expect(page).to have_content("Job Count By Location:")
     expect(page).to have_content("Top Three Companies by Average Level of Interest:")
-    expect(page).to have_content("Denver"=>3)
-    expect(page).to have_content("Orlando"=>3)
+  end
+  scenario "visits dashboard and clicks on company link" do 
+    company = create(:company)
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job2 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job3 = company.jobs.create!(title: "Developer", level_of_interest: 50, city: "Orlando")
+    job4 = company.jobs.create!(title: "Developer", level_of_interest: 50, city: "Orlando")
+
+    visit '/dashboard'
+
+    click_on "All Companies"
+
+    expect(current_path).to eq('/companies')
+  end
+  scenario "visits dashboard and clicks on categories link" do 
+    company = create(:company)
+    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job1 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job2 = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    job3 = company.jobs.create!(title: "Developer", level_of_interest: 50, city: "Orlando")
+    job4 = company.jobs.create!(title: "Developer", level_of_interest: 50, city: "Orlando")
+
+    visit '/dashboard'
+
+    click_on "All Categories"
+
+    expect(current_path).to eq('/categories')
   end
 end
